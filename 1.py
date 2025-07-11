5d7ac4fa4457629e8869949ae16e2e355b817353
맹구 <yomyeongju@naver.com>
2025-07-11 12:47:38 +0900

    첫 번째 커밋

---
+++ 1.py
@@ -0,0 +1,25 @@
+# git: 코드관리, 저장, 형상관리, 버전관리
+# github: 클라우드 
+
+# 로컬( 내 컴퓨터 )에서 먼저 commit
+# 깃허브에 저장한다
+
+# 초기화 루트 폴더에 초기화한다
+# git init
+
+# git status 현재 상태 확인
+
+# git commit 
+
+# git commit 하는 법
+# 1. git add [파일명] 내가 원하는 파일만
+# 1-1. git add. > 폴더 내 모든 파일
+
+# 커밋
+# 2. git commit -m "첫 번째 커밋입니다"
+
+# E-mail 설정
+# git config --global user.email "you@example.com"
+
+# 이름 설정
+# git config --global user.name "이름"
